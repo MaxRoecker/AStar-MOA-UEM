7187477a77d1f31e166510147a0745f3481c9a20
jlramalheira <jlramalheira@gmail.com>
2014-09-22 11:01:40 -0300

    Primeiro comit

---
+++ AStar-MOA-UEM/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: joao
+ *
+ * Created on 22 de Setembro de 2014, 11:00
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
